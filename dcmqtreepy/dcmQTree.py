cdbc05a2d287d0ed77ada5e3d2f28fb1f735fe12
Stuart Swerdloff <sjswerdloff@gmail.com>
2024-06-24 00:31:00 +1200

    reorganized directories added support for handling known private elements add explicit inclusion of impac private elements in private dictionary

---
+++ dcmqtreepy/dcmQTree.py
@@ -0,0 +1,293 @@
+# This Python file uses the following encoding: utf-8
+import logging
+import sys
+from datetime import datetime
+from pathlib import Path
+from typing import List
+
+import pydicom
+import pydicom.config
+import pydicom.datadict
+import pydicom.dataset
+import pydicom.valuerep
+import tomli
+from pydicom import DataElement, Dataset, Sequence, dcmread, dcmwrite
+from pydicom.valuerep import VR
+from pynetdicom.presentation import build_context
+from PySide6.QtCore import QDateTime, Qt, Slot  # pylint: disable=no-name-in-module
+from PySide6.QtWidgets import (  # pylint: disable=no-name-in-module
+    QApplication,
+    QFileDialog,
+    QHeaderView,
+    QListWidget,
+    QListWidgetItem,
+    QMainWindow,
+    QMessageBox,
+    QTreeWidget,
+    QTreeWidgetItem,
+    QTreeWidgetItemIterator,
+    QWidget,
+)
+
+from dcmqtreepy import impac_privates
+from dcmqtreepy.mainwindow import Ui_MainWindow
+
+
+class DCMQtreePy(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.dcm_tree_widget = self.ui.treeWidget
+        self.dcm_tree_widget.editTriggers = self.dcm_tree_widget.EditTrigger.NoEditTriggers
+        header = self.dcm_tree_widget.header()
+        header.setMinimumSectionSize(40)  # characters or pixels?
+        header.setDefaultSectionSize(200)  # pixels
+        # header.setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        # header.setStretchLastSection(False)
+        # header.setSectionResizeMode(5, QHeaderView.Stretch)
+        self.dcm_tree_widget.itemDoubleClicked.connect(self.on_tree_widget_item_double_clicked)
+        self.ui.listWidget.itemSelectionChanged.connect(self.on_item_selection_changed)
+        self.ui.actionOpen.triggered.connect(self.on_file_open)
+        self.ui.actionSave_As.triggered.connect(self.on_file_save_as)
+        self.previous_path = Path().home()
+        self.previous_save_path = Path().home()
+        self.current_dataset = Dataset()
+        pydicom.config.Settings.writing_validation_mode = pydicom.config.RAISE
+        pydicom.datadict.add_private_dict_entries("IMPAC", impac_privates.impac_private_dict)
+
+    def _populate_tree_widget_item_from_dataset(self, parent: QTreeWidgetItem | QTreeWidget, ds: Dataset):
+        for elem in ds:
+            if elem.VR != VR.SQ:
+                tree_child_item = QTreeWidgetItem(parent)
+                tree_child_item.setFlags(Qt.ItemFlag.ItemIsEditable | Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsEnabled)
+                tree_child_item.setText(0, str(elem.tag))
+                tree_child_item.setText(1, elem.name)
+                if elem.VR not in [VR.OB, VR.OW, VR.OB_OW, VR.OD, VR.OF]:
+                    if elem.value is None:
+                        tree_child_item.setText(2, "")
+                    else:
+                        tree_child_item.setText(2, str(elem.value))
+                else:
+                    logging.warning("Need to stash away OB/OW type values")
+                tree_child_item.setText(3, str(elem.VR))
+                tree_child_item.setText(4, elem.keyword)
+            else:
+                tree_child_item = QTreeWidgetItem(parent)
+                tree_child_item.setText(0, str(elem.tag))
+                tree_child_item.setText(1, elem.name)
+                tree_child_item.setText(3, str(elem.VR))
+                tree_child_item.setText(4, elem.keyword)
+                seq_item_count = 0
+                for seq_item in elem:
+                    seq_item_count += 1
+                    seq_child_item = QTreeWidgetItem(tree_child_item)
+                    seq_child_item.setText(0, str(elem.tag))
+                    seq_child_item.setText(1, elem.name)
+                    seq_child_item.setText(2, str(seq_item_count))
+                    seq_child_item.setText(3, str(elem.VR))
+                    seq_child_item.setText(4, elem.keyword)
+                    self._populate_tree_widget_item_from_dataset(seq_child_item, seq_item)
+
+    def _populate_dataset_from_tree_widget_item(
+        self,
+        parent_ds: Dataset | Sequence | list,
+        tree_widget_item: QTreeWidgetItem,
+        private_block: pydicom.dataset.PrivateBlock = None,
+    ) -> pydicom.dataset.PrivateBlock:
+        tag_as_string = tree_widget_item.text(0)
+        name_as_string = tree_widget_item.text(1)
+        value_as_string = tree_widget_item.text(2)
+        vr_as_string = tree_widget_item.text(3)
+        tag = self._convert_tag_as_string_to_tuple(tag_as_string)
+        is_private = False
+
+        # if tag[0] == 0x300a and tag[1] == 0x0782:
+        #     raise ValueError("There is no 0x300a0782 element")
+        if tag[0] % 2 == 1:
+            # print(f"Private Element: {tag}")
+            is_private = True
+            group = tag[0]
+            private_block_byte = tag[1] % 256  # lower 8 bits...
+            if private_block_byte == 0x10:
+                private_creator = value_as_string
+                private_block = parent_ds.private_block(group, private_creator, create=True)
+                return private_block
+                # parent_ds.private_creators()
+        else:
+            private_block = None
+        tag = tree_widget_item.text(4)  # keyword
+
+        if vr_as_string == "SQ":
+            if value_as_string is None or value_as_string == "":
+                #    seq_elem = DataElement(tag=tag,VR=vr_as_string,value=Sequence())
+                #    parent_ds.add(seq_elem)
+                parent_ds.add(DataElement(tag=tag, VR=vr_as_string, value=None))
+                # parent_ds[tag]= Sequence()
+                seq_elem = parent_ds[tag]
+                for child_index in range(tree_widget_item.childCount()):
+                    child = tree_widget_item.child(child_index)
+                    private_block = self._populate_dataset_from_tree_widget_item(
+                        parent_ds=seq_elem, tree_widget_item=child, private_block=private_block
+                    )
+            else:
+                item_number = int(value_as_string)
+                child_ds = Dataset()
+                my_list = parent_ds.value
+                my_list.append(child_ds)
+                for child_index in range(tree_widget_item.childCount()):
+                    child = tree_widget_item.child(child_index)
+                    private_block = self._populate_dataset_from_tree_widget_item(
+                        parent_ds=child_ds, tree_widget_item=child, private_block=private_block
+                    )
+
+        elif len(vr_as_string) > 0:  # if there isn't a VR, there's no point in encoding
+            if len(value_as_string) > 0 and value_as_string[0] == "[":
+                my_list_of_values = value_as_string.split("[")[1].split("]")[0].split(",")
+                cast_list_of_values = my_list_of_values
+                if vr_as_string in [VR.IS, VR.SS, VR.US]:
+                    cast_list_of_values = [int(x) for x in my_list_of_values]
+                elif vr_as_string in [VR.DS, VR.FL, VR.FD]:
+                    cast_list_of_values = [float(x) for x in my_list_of_values]
+                else:
+                    cast_list_of_values = [x.strip('"').strip(" ").strip("'") for x in my_list_of_values]
+
+                if is_private:
+                    if private_block is not None:
+                        private_block.add_new(private_block_byte, vr_as_string, cast_list_of_values)
+                    else:
+                        logging.warning(
+                            f"Private element {group:04x},{private_block_byte:02x} found with no private block parent"
+                        )
+                        known_private_creators = parent_ds.private_creators(group)
+                        if len(known_private_creators) == 1:
+                            known_private_creator = known_private_creators[0]
+                            private_block = parent_ds.private_block(group, private_creator=known_private_creator, create=True)
+                            private_block.add_new(private_block_byte, vr_as_string, cast_value)
+                            logging.warning("Found a private creator to attach private element to")
+                        else:
+                            logging.error(
+                                f"Unable to find private creator for Private element {group:04x},{private_block_byte:02x} element can not be copied/saved"
+                            )
+
+                else:
+                    elem = DataElement(tag=tag, VR=vr_as_string, value=cast_list_of_values)
+            else:
+                try:
+                    cast_value = value_as_string
+                    if len(value_as_string) > 0:
+                        if vr_as_string in [VR.SS, VR.US]:
+                            cast_value = int(value_as_string)
+                        elif vr_as_string in [VR.FL, VR.FD]:
+                            cast_value = float(value_as_string)
+                    elif vr_as_string in [VR.SS, VR.US, VR.FL, VR.FD]:
+                        cast_value = None
+                    if is_private:
+                        if private_block is not None:
+                            private_block.add_new(private_block_byte, vr_as_string, cast_value)
+                        else:
+                            logging.warning(
+                                f"Private element {group:04x},{private_block_byte:02x} found with no private block parent"
+                            )
+                            known_private_creators = parent_ds.private_creators(group)
+                            if len(known_private_creators) == 1:
+                                known_private_creator = known_private_creators[0]
+                                private_block = parent_ds.private_block(
+                                    group, private_creator=known_private_creator, create=True
+                                )
+                                private_block.add_new(private_block_byte, vr_as_string, cast_value)
+                                logging.warning("Found a private creator to attach private element to")
+                            else:
+                                logging.error(
+                                    f"Unable to find private creator for Private element {group:04x},{private_block_byte:02x} element can not be copied/saved"
+                                )
+
+                    else:
+                        elem = DataElement(tag=tag, VR=vr_as_string, value=cast_value)
+                except ValueError as encoding_error:
+                    logging.error(f"{tag} {tag_as_string} {encoding_error}")
+            if not is_private:
+                parent_ds.add(elem)
+        return private_block
+
+    def _convert_tag_as_string_to_tuple(self, tag_as_string: str) -> tuple[int, int]:
+        first_split = tag_as_string.split("(")[1]
+        group = first_split.split(",")[0]
+        elem_hex_as_string = first_split.split(",")[1].split(")")[0]
+        return (int(group, 16), int(elem_hex_as_string, 16))
+
+    def _isEditable(self, column: int) -> bool:
+        if column == 2:
+            return True
+        else:
+            return False
+
+    @Slot()
+    def on_item_selection_changed(self):
+        current_item = self.ui.listWidget.currentItem()
+        self.populate_tree_widget_from_file(current_item.text())
+
+    @Slot()
+    def on_tree_widget_item_double_clicked(self, item: QTreeWidgetItem, column: int):
+        if self._isEditable(column):
+            self.dcm_tree_widget.editItem(item, column)
+        else:
+            print(f"Column {column} is not editable")
+
+    def on_file_open(self):
+        previous_path = self.previous_path
+        file_name, ok = QFileDialog.getOpenFileName(self, "Open DICOM File", str(previous_path), "DICOM Files (*.dcm)")
+        if file_name:
+            file_list_item = QListWidgetItem(str(file_name))
+            self.ui.listWidget.addItem(file_list_item)
+            self.populate_tree_widget_from_file(file_name)
+
+    def populate_tree_widget_from_file(self, file_name: str | Path):
+        if file_name:
+            path = Path(file_name)
+            self.previous_path = path.parent
+            ds = dcmread(path, force=True)
+            # ds.remove_private_tags() # temporarily, until save as is working.
+            self.current_dataset = ds
+            self.dcm_tree_widget.clear()
+            tree_child_item = QTreeWidgetItem(self.dcm_tree_widget)
+            context = build_context(ds.SOPClassUID)
+            abstract_syntax = str(context).splitlines()[0].split(sep=":")[1]
+            tree_child_item.setText(0, abstract_syntax)
+            self._populate_tree_widget_item_from_dataset(parent=tree_child_item, ds=ds)
+            tree_child_item.setExpanded(True)
+
+    def on_file_save_as(self):
+        save_path = self.previous_save_path
+        file_name, ok = QFileDialog.getSaveFileName(self, "Save DICOM File", str(save_path), "DICOM Files (*.dcm)")
+        if len(file_name) == 0:
+            return
+        path = Path(file_name)
+        self.previous_save_path = path.parent
+        iterator = QTreeWidgetItemIterator(self.dcm_tree_widget)
+        tree_child_item = iterator.__next__().value()
+        modified_ds = Dataset()
+
+        # modified_ds.is_little_endian = True
+
+        for child_index in range(tree_child_item.childCount()):
+            child = tree_child_item.child(child_index)
+            self._populate_dataset_from_tree_widget_item(parent_ds=modified_ds, tree_widget_item=child)
+
+        if "PixelData" in self.current_dataset:
+            modified_ds["PixelData"] = self.current_dataset["PixelData"]
+        # modified_ds.fix_meta_info(enforce_standard=False)
+        modified_ds.ensure_file_meta()
+        #
+        modified_ds.is_implicit_VR = False
+        modified_ds.file_meta.TransferSyntaxUID = "1.2.840.10008.1.2.1"
+        # del modified_ds[0x300a0782]
+        # modified_ds.remove_private_tags() # temporary... first get save as working for public elements
+        dcmwrite(Path(file_name), modified_ds, write_like_original=False)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = DCMQtreePy()
+    widget.show()
+    sys.exit(app.exec())
---
+++ dcmqtreepy/mainwindow.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (
+    QCoreApplication,
+    QDate,
+    QDateTime,
+    QLocale,
+    QMetaObject,
+    QObject,
+    QPoint,
+    QRect,
+    QSize,
+    Qt,
+    QTime,
+    QUrl,
+)
+from PySide6.QtGui import (
+    QAction,
+    QBrush,
+    QColor,
+    QConicalGradient,
+    QCursor,
+    QFont,
+    QFontDatabase,
+    QGradient,
+    QIcon,
+    QImage,
+    QKeySequence,
+    QLinearGradient,
+    QPainter,
+    QPalette,
+    QPixmap,
+    QRadialGradient,
+    QTransform,
+)
+from PySide6.QtWidgets import (
+    QAbstractItemView,
+    QAbstractScrollArea,
+    QApplication,
+    QFormLayout,
+    QGroupBox,
+    QHeaderView,
+    QListWidget,
+    QListWidgetItem,
+    QMainWindow,
+    QMenu,
+    QMenuBar,
+    QScrollArea,
+    QSizePolicy,
+    QStatusBar,
+    QTreeWidget,
+    QTreeWidgetItem,
+    QWidget,
+)
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.actionFile = QAction(MainWindow)
+        self.actionFile.setObjectName("actionFile")
+        self.actionOpen = QAction(MainWindow)
+        self.actionOpen.setObjectName("actionOpen")
+        self.actionSave = QAction(MainWindow)
+        self.actionSave.setObjectName("actionSave")
+        self.actionSave_As = QAction(MainWindow)
+        self.actionSave_As.setObjectName("actionSave_As")
+        self.actionAdd_Element = QAction(MainWindow)
+        self.actionAdd_Element.setObjectName("actionAdd_Element")
+        self.actionAdd_Private_Element = QAction(MainWindow)
+        self.actionAdd_Private_Element.setObjectName("actionAdd_Private_Element")
+        self.central_widget = QWidget(MainWindow)
+        self.central_widget.setObjectName("central_widget")
+        self.formLayout_5 = QFormLayout(self.central_widget)
+        self.formLayout_5.setObjectName("formLayout_5")
+        self.groupBox = QGroupBox(self.central_widget)
+        self.groupBox.setObjectName("groupBox")
+        self.formLayout_2 = QFormLayout(self.groupBox)
+        self.formLayout_2.setObjectName("formLayout_2")
+        self.scrollArea = QScrollArea(self.groupBox)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 744, 213))
+        self.formLayout = QFormLayout(self.scrollAreaWidgetContents)
+        self.formLayout.setObjectName("formLayout")
+        self.listWidget = QListWidget(self.scrollAreaWidgetContents)
+        self.listWidget.setObjectName("listWidget")
+        self.listWidget.setAutoFillBackground(True)
+        self.listWidget.setSelectionRectVisible(True)
+
+        self.formLayout.setWidget(0, QFormLayout.SpanningRole, self.listWidget)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.formLayout_2.setWidget(0, QFormLayout.SpanningRole, self.scrollArea)
+
+        self.formLayout_5.setWidget(0, QFormLayout.SpanningRole, self.groupBox)
+
+        self.groupBox_2 = QGroupBox(self.central_widget)
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.formLayout_3 = QFormLayout(self.groupBox_2)
+        self.formLayout_3.setObjectName("formLayout_3")
+        self.scrollArea_2 = QScrollArea(self.groupBox_2)
+        self.scrollArea_2.setObjectName("scrollArea_2")
+        self.scrollArea_2.setWidgetResizable(True)
+        self.scrollAreaWidgetContents_2 = QWidget()
+        self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
+        self.scrollAreaWidgetContents_2.setGeometry(QRect(0, 0, 744, 210))
+        self.formLayout_4 = QFormLayout(self.scrollAreaWidgetContents_2)
+        self.formLayout_4.setObjectName("formLayout_4")
+        self.treeWidget = QTreeWidget(self.scrollAreaWidgetContents_2)
+        __qtreewidgetitem = QTreeWidgetItem()
+        __qtreewidgetitem.setText(4, "5")
+        __qtreewidgetitem.setText(3, "4")
+        __qtreewidgetitem.setText(2, "Value")
+        __qtreewidgetitem.setText(1, "Name")
+        __qtreewidgetitem.setText(0, "Tag")
+        self.treeWidget.setHeaderItem(__qtreewidgetitem)
+        self.treeWidget.setObjectName("treeWidget")
+        self.treeWidget.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.treeWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.treeWidget.setAlternatingRowColors(True)
+        self.treeWidget.setIndentation(4)
+        self.treeWidget.setColumnCount(5)
+
+        self.formLayout_4.setWidget(0, QFormLayout.SpanningRole, self.treeWidget)
+
+        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents_2)
+
+        self.formLayout_3.setWidget(0, QFormLayout.SpanningRole, self.scrollArea_2)
+
+        self.formLayout_5.setWidget(1, QFormLayout.SpanningRole, self.groupBox_2)
+
+        MainWindow.setCentralWidget(self.central_widget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName("menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 37))
+        self.menuMain_Window = QMenu(self.menubar)
+        self.menuMain_Window.setObjectName("menuMain_Window")
+        self.menuEdit = QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuMain_Window.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
+        self.menuMain_Window.addAction(self.actionOpen)
+        self.menuMain_Window.addAction(self.actionSave)
+        self.menuMain_Window.addAction(self.actionSave_As)
+        self.menuEdit.addAction(self.actionAdd_Element)
+        self.menuEdit.addAction(self.actionAdd_Private_Element)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", "MainWindow", None))
+        self.actionFile.setText(QCoreApplication.translate("MainWindow", "File", None))
+        self.actionOpen.setText(QCoreApplication.translate("MainWindow", "Open", None))
+        self.actionSave.setText(QCoreApplication.translate("MainWindow", "Save", None))
+        self.actionSave_As.setText(QCoreApplication.translate("MainWindow", "Save As...", None))
+        self.actionAdd_Element.setText(QCoreApplication.translate("MainWindow", "Add Element", None))
+        self.actionAdd_Private_Element.setText(QCoreApplication.translate("MainWindow", "Add Private Element", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", "GroupBox", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", "GroupBox", None))
+        self.menuMain_Window.setTitle(QCoreApplication.translate("MainWindow", "File", None))
+        self.menuEdit.setTitle(QCoreApplication.translate("MainWindow", "Edit", None))
+
+    # retranslateUi
